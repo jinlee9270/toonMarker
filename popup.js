17e1b28f87711975414faac12c37eec8fbc7ff8d
jinlee <vito9270@naver.com>
2022-07-27 00:13:25 +0900

    배경색 변경 기능 테스트

--- popup.js
+++ popup.js
@@ -1,5 +1,27 @@
-console.log(document.getElementById('btn'))
+document.getElementById('id_Red').onclick = () => {
+    chrome.tabs.query({active: true, currentWindow:true}, (tabs) =>
+    {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: setBackGroundColorRed
+        });
+    });
+}
 
-function setBackGroundColorBlue() {
+document.getElementById('id_Blue').onclick = () => {
+    chrome.tabs.query({active: true, currentWindow:true}, (tabs) =>
+    {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: setBackGroundColorBlue
+        });
+    });
+}
+
+function setBackGroundColorRed(){
+    document.body.style.backgroundColor = 'red'
+}
+
+function setBackGroundColorBlue(){
     document.body.style.backgroundColor = 'blue'
 }
