ba7d57d8c5f2b20cdf49794c325b69f9a9348ac0
jinlee <vito9270@naver.com>
2022-07-28 00:44:51 +0900

    popup.html 내title list저장

--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
 let titles = []
+
 document.addEventListener('DOMContentLoaded', function(){
     let btn = document.getElementById("btn")
     btn.addEventListener("click", function(){
         let title = document.getElementById("inputText").value
         titles.push(title)
-        // console.log(titles)
+
+        ul.innerText = ""
+
+        titles.forEach((toon) => {
+            console.log(toon)
+            const item = document.createElement('li')
+            item.innerText = toon
+            ul.appendChild(item)
+        })
     });
 });
