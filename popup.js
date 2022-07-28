32d2e101aae5d54385bd76a215cfd19b14c47b3e
jinlee <vito9270@naver.com>
2022-07-28 14:44:43 +0900

    enter event 추가 및 함수 분리

--- popup.js
+++ popup.js
@@ -2,17 +2,38 @@ let titles = []
 
 document.addEventListener('DOMContentLoaded', function(){
     let btn = document.getElementById("btn")
-    btn.addEventListener("click", function(){
-        let title = document.getElementById("inputText").value
+    let inputText = document.getElementById("inputText")
+    btn.addEventListener("click", addList)
+    inputText.addEventListener("keyup", function(e) {
+        if (e.key == 'Enter'){
+            addList()
+        }
+    })
+
+});
+
+function addList(){
+    let title = document.getElementById("inputText").value
+        if (titles.includes(title)) {
+            let options = {
+                type: "basic",
+                title: "이미 있는 제목입니다.",
+                message: '/images/8080.webp',
+                iconUrl: ''
+            }
+            chrome.notifications.create(options)
+        }
+        else {
         titles.push(title)
 
         ul.innerText = ""
-
+        
         titles.forEach((toon) => {
             console.log(toon)
             const item = document.createElement('li')
             item.innerText = toon
             ul.appendChild(item)
+
         })
-    });
-});
+    }
+}
