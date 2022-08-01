06c6966ce7bdf6c8aaf311b27e72d77133388889
jinlee <vito9270@naver.com>
2022-08-01 14:09:27 +0900

    string parsing 배열로 받음

--- content_scripts.js
+++ content_scripts.js
@@ -1,2 +1,12 @@
-let bodyText = document.querySelector("body").innerText
-console.log(bodyText)
+let bodyText = document.querySelectorAll('li')
+let arr = Array.from(bodyText)
+// console.log(bodyText)
+// console.log(arr)
+for (let i = 0; i < arr.length ;i++){
+    // console.log(arr[i].innerText)
+    if(arr[i].innerText === '참교육'){
+        arr[i].style.color = '#ff0000'
+        console.log("여기있다")
+        console.log(arr[i].style)
+    }
+}
