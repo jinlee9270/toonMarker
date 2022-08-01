55532f4ec3c49048a86e084c881227c1e64acebb
jinlee <vito9270@naver.com>
2022-08-01 19:44:04 +0900

    글자색 변환 테스트

--- content_scripts.js
+++ content_scripts.js
@@ -1,12 +1,13 @@
-let bodyText = document.querySelectorAll('li')
+let bodyText = document.querySelectorAll('a')
 let arr = Array.from(bodyText)
 // console.log(bodyText)
 // console.log(arr)
 for (let i = 0; i < arr.length ;i++){
     // console.log(arr[i].innerText)
     if(arr[i].innerText === '참교육'){
+        console.log(arr[i].style.color)
         arr[i].style.color = '#ff0000'
         console.log("여기있다")
-        console.log(arr[i].style)
+        console.log(arr[i].style.color)
     }
 }
