016ac75b9b60326754eb080abc253845e298c802
jinlee <vito9270@naver.com>
2022-08-01 19:52:43 +0900

    img grayscale 적용 테스트

--- content_scripts.js
+++ content_scripts.js
@@ -1,13 +1,9 @@
-let bodyText = document.querySelectorAll('a')
+let bodyText = document.querySelectorAll('img')
 let arr = Array.from(bodyText)
-// console.log(bodyText)
-// console.log(arr)
+
 for (let i = 0; i < arr.length ;i++){
-    // console.log(arr[i].innerText)
-    if(arr[i].innerText === '참교육'){
+    if(arr[i].title === '참교육'){
         console.log(arr[i].style.color)
-        arr[i].style.color = '#ff0000'
-        console.log("여기있다")
-        console.log(arr[i].style.color)
+        arr[i].style.filter = 'grayscale(100%)'
     }
 }
