6f7e5ce66213490f4f2cf45fdd80919610052d22
jinlee <vito9270@naver.com>
2022-08-02 22:48:55 +0900

    input 값 storage 적용 테스트

--- content_scripts.js
+++ content_scripts.js
@@ -2,8 +2,7 @@ let bodyText = document.querySelectorAll('img')
 let arr = Array.from(bodyText)
 
 for (let i = 0; i < arr.length ;i++){
-    if(arr[i].title === '참교육'){
-        console.log(arr[i].style.color)
+    if(arr[i].title !== '참교육'){
         arr[i].style.filter = 'grayscale(100%)'
     }
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 let titles = []
 
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    let tab = tabs[0].url
+    chrome.scripting.executeScript({
+        target: {tabId: tab},
+        file: 'content_scripts.js'
+    })
+});
+
 document.addEventListener('DOMContentLoaded', function(){
     let btn = document.getElementById("btn")
     let inputText = document.getElementById("inputText")
@@ -14,35 +22,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function addList(){
     let title = document.getElementById("inputText").value
-        if (titles.includes(title)) {
-            let options = {
-                type: "basic",
-                title: '이미 있는 제목',
-                message: title,
-                iconUrl: '/images/8080.webp'
-            }
-            chrome.notifications.create(options)
-        }
-        else {
-        titles.push(title)
-
-        ul.innerText = ""
-        
-        titles.forEach((toon) => {
-            console.log(toon)
-            const item = document.createElement('li')
-            item.innerText = toon
-            ul.appendChild(item)
-
-        })
-    }
+    chrome.storage.local.set({ title }, function(){
+        console.log(title)
+        chrome.storage.local.get("title", function (value) {
+            console.log(value);
+        });
+    })
+        // if (titles.includes(title)) {
+        //     let options = {
+        //         type: "basic",
+        //         title: '이미 있는 제목',
+        //         message: title,
+        //         iconUrl: '/images/8080.webp'
+        //     }
+        //     chrome.notifications.create(options)
+        // }
+        // else {
+        //     titles.push(title)
+        //     ul.innerText = ""
+            
+        //     titles.forEach((toon) => {
+        //         const item = document.createElement('li')
+        //         item.innerText = toon
+        //         ul.appendChild(item)
+        //     })
+        // }
 }
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    let tab = tabs[0].url
-    chrome.scripting.executeScript({
-        target: {tabId: tab},
-        file: 'content_scripts.js'
-    })
-});
+
 
