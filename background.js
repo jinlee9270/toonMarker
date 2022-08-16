e2ae5c51dc10300b436b8b7ebec8e86cdf7f39d6
jinlee <vito9270@naver.com>
2022-08-16 22:04:59 +0900

    get url not tabID

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-    console.log('[background] chrome.runtime.onMessage.addListener()');
+    console.log('[background] chrome.runtime.onMessage.addListener()')
 
     if (request.greeting === 'backgroundhello') {
-        console.log("[background] request:" + request.greeting);
+        console.log("[background] request:" + request.greeting)
         // TODO
-        sendResponse({ farewell: 'contentgoodbye' });
+        sendResponse({ farewell: 'contentgoodbye' })
     }
-});
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        let tabURL = tabs[0].url;
+        console.log("tabURL",tabURL);
+    });
+})
--- content.js
+++ content.js
@@ -3,17 +3,18 @@ let arr = Array.from(bodyText)
 
 chrome.storage.local.get(null, function(items){
     const mykey = Object.keys(items)
-    console.log("mykey",mykey)
-    console.log('arr', arr)
+    // console.log("mykey",mykey)
+    // console.log('arr', arr)
     for (let i = 0; i < arr.length ;i++){
         if(!mykey.includes(arr[i].title)){
-            console.log("arr[i]",arr[i])
+            // console.log("arr[i]",arr[i])
             arr[i].style.filter = 'grayscale(100%)'
         }
     }
 })
 
 chrome.runtime.sendMessage({greeting: 'backgroundhello'}, (response) => {
-    console.log("[contentscript] chrome.runtime.sendMessage()");
-    console.log(response.farewell);
-});
+    console.log("[contentscript] chrome.runtime.sendMessage()")
+    console.log(response.farewell)
+
+})
