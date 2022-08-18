db638497869941731752398d768dc9df829fd19a
jinlee <vito9270@naver.com>
2022-08-18 18:43:54 +0900

    popup ui 수정

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
-chrome.runtime.onInstalled.addListener(details => {
-    //json 형식으로 data 저장
-    if(details.reason === "install")
-    {
-        console.log("install")
-    }
-    else if(details.reason === "update")
-    {
-        console.log("update")
-    }
-})
-
-
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
     console.log('[background] chrome.runtime.onMessage.addListener()')
