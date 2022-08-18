7cfa4c0b6f079aa7e4b10d129c17a2334af8792e
jinlee <vito9270@naver.com>
2022-08-19 00:00:55 +0900

    css 적용 확인

--- content.js
+++ content.js
@@ -12,27 +12,28 @@ for (let i = 0; i < arr.length; i++){
             // console.log("database",database)
         }
     }
-    chrome.storage.local.set({"database":database})
+    chrome.storage.local.set({["database"]:database})
 }
 
-chrome.storage.local.get(null,(database) => {
-    console.log("database", database)
+// chrome.storage.local.get(null,(database) => {
+//     console.log("database", database)
+// })
+
+
+chrome.storage.local.get(null, (items) => {
+    Object.entries(database).forEach((element) => {
+        // console.log(element)
+        // console.log(element[1].preWatch, element[1].title)
+        if (element[1].preWatch.length === 0){
+            // console.log(element[1].title)
+            for(let i = 0;i < arr.length;i++){
+                let title = arr[i].alt
+                if (title === element[1].title){
+                    console.log(title, element[1].title)
+                    arr[i].style.filter = "grayscale(100%)"
+                }
+            }
+        }
+    })
 })
 
-
-// chrome.storage.local.get(null, function(items){
-//     const mykey = Object.keys(items)
-//     // console.log("mykey",mykey)
-//     // console.log('arr', arr)
-//     for (let i = 0; i < arr.length ;i++){
-//         if(!mykey.includes(arr[i].title)){
-//             // console.log("arr[i]",arr[i])
-//             arr[i].style.filter = 'grayscale(100%)'
-//         }
-//     }
-// })
-
-// chrome.runtime.sendMessage({greeting: 'backgroundhello'}, (response) => {
-//     console.log("[contentscript] chrome.runtime.sendMessage()")
-//     console.log(response.farewell)
-// })
