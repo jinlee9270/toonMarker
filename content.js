86de89f49bbac48ecf12904d47c67192447d99e6
jinlee <vito9270@naver.com>
2022-08-18 22:47:44 +0900

    data input 조건작성

--- content.js
+++ content.js
@@ -1,16 +1,20 @@
 let bodyText = document.querySelectorAll('li div a img')
 let arr = Array.from(bodyText)
 // console.log("content",arr)
-
+let database = []
 for (let i = 0; i < arr.length; i++){
     let title = arr[i].alt
     const temp = arr[i].currentSrc.split("/")
-    let titleId = temp[5]
-    const preView = null
-    if (titleId !== undefined && titleId.length === 6){
-        console.log(title)
+    let id = temp[5]
+    if (id !== undefined && id.length === 6){
+        if (!database.find(obj => obj.id === id)) {
+            database.push({"id":id, "title":title, "preWatch":[]})
+            console.log("database",database)
+        }
     }
+    // chrome.storage.local.set(database, JSON.stringify(database)
 }
+
 // chrome.storage.local.get(null, function(items){
 //     const mykey = Object.keys(items)
 //     // console.log("mykey",mykey)
