aa04908df93a3565b14722c46bfdf765f37329a5
jinlee <vito9270@naver.com>
2022-08-18 19:09:18 +0900

    parsing test

--- content.js
+++ content.js
@@ -1,19 +1,29 @@
-let bodyText = document.querySelectorAll('img')
+let bodyText = document.querySelectorAll('li div a img')
 let arr = Array.from(bodyText)
 
-chrome.storage.local.get(null, function(items){
-    const mykey = Object.keys(items)
-    // console.log("mykey",mykey)
-    // console.log('arr', arr)
-    for (let i = 0; i < arr.length ;i++){
-        if(!mykey.includes(arr[i].title)){
-            // console.log("arr[i]",arr[i])
-            arr[i].style.filter = 'grayscale(100%)'
-        }
-    }
-})
+console.log("content",arr)
 
-chrome.runtime.sendMessage({greeting: 'backgroundhello'}, (response) => {
-    console.log("[contentscript] chrome.runtime.sendMessage()")
-    console.log(response.farewell)
-})
+for (let i = 0; i <arr.length;i++){
+    let title = arr[i].alt
+    const temp = arr[i].currentSrc.split("/")
+    let titleId = temp[5]
+    
+    console.log(arr[i].alt)
+    console.log(temp[5])
+}
+// chrome.storage.local.get(null, function(items){
+//     const mykey = Object.keys(items)
+//     // console.log("mykey",mykey)
+//     // console.log('arr', arr)
+//     for (let i = 0; i < arr.length ;i++){
+//         if(!mykey.includes(arr[i].title)){
+//             // console.log("arr[i]",arr[i])
+//             arr[i].style.filter = 'grayscale(100%)'
+//         }
+//     }
+// })
+
+// chrome.runtime.sendMessage({greeting: 'backgroundhello'}, (response) => {
+//     console.log("[contentscript] chrome.runtime.sendMessage()")
+//     console.log(response.farewell)
+// })
