5bdd1b3f0b430eea00ee5dc3fb79baeeb47e8021
jinlee <vito9270@naver.com>
2022-09-14 19:59:04 +0900

    addPreWatch 함수명 변수명 변경

--- content.js
+++ content.js
@@ -21,31 +21,25 @@ const makeItem = () => {
 }
 
 chrome.runtime.sendMessage({cmd: "getURL"}, (response) => {
-    // console.log("getURL");
-    // console.log("content",response.frompopup)
     
     const url = new URL(response.frompopup)
     const urlHref = url.href
     const titleId =  url.searchParams.get('titleId')
     const no = url.searchParams.get('no')
-    // console.log("no",no)
     const pathname = url.pathname
-    // console.log("pathname",pathname)
 
     // 메인 페이지 접속시에만 작동하게 설정
     // url parsing 에서 id 추출방법은 맞지 않아서 split 하는 방법으로 구현
-    // 변수 명 바꾸기  
     if (urlHref === "https://comic.naver.com/webtoon/weekday") {
-        chrome.storage.local.get(null, (item) => {
-            let bodyText = document.querySelectorAll('li div a img')
-            let arr = Array.from(bodyText)
-            Object.entries(item).forEach((element) => {
-                if (element[1].preWatch.length === 0){
-                    // console.log(element[1])
-                    for(let i = 0;i < arr.length;i++){
-                        let title = arr[i].alt
-                        if (title === element[1].title){
-                            arr[i].style.filter = "grayscale(100%)"
+        chrome.storage.local.get(null, (subObjs) => {
+            let imgNodes = document.querySelectorAll('li div a img')
+            let imgList = Array.from(imgNodes)
+            Object.entries(subObjs).forEach((subObj) => {
+                if (subObj[1].preWatch.length === 0){
+                    for(let i = 0;i < imgList.length;i++){
+                        let title = imgList[i].alt
+                        if (title === subObj[1].title){
+                            imgList[i].style.filter = "grayscale(100%)"
                         }
                     }
                 }
@@ -55,42 +49,37 @@ chrome.runtime.sendMessage({cmd: "getURL"}, (response) => {
 
     if (titleId && no && pathname == "/webtoon/detail") {
         const title = document.querySelector('div.detail h2 span.title').innerText
-        // console.log('title1',title)
-        addPreWatch(titleId, title, no)
+        addPreEpi(titleId, title, no)
     }
 
     if (url.href == "https://comic.naver.com/webtoon/weekday") {
-        console.log("home")
         makeItem()
     }
 
     else if (url.href.includes("https://comic.naver.com/webtoon/list?titleId=")) {
         console.log("list page")
-        // 봤던 화수에 대해 색을 다르게 바꾸는 함수 생성
+        // 봤던 화수에 대해 색을 다르게 바꾸는 함수 호출
         isWatched(titleId)
     }
 })
 
 // addPreWatch 함수이름 변경할 것 isWatched과 비슷하게 일치시킬 것
 // target, item, watchedNumbers도 명확한 의미로 변경할 것
-function addPreWatch (titleId, title, no){
-    chrome.storage.local.get(titleId, (target) => {
-        Object.entries(target).forEach((item) => {
-            // console.log(item)
-            if (item[1]) {
-                const watchedNumbers = item[1].preWatch
-                // console.log(watchedNumbers)
-                if (!watchedNumbers.includes(no)) {
-                    watchedNumbers.push(no)
-    
-                    chrome.storage.local.set({[titleId]:{"id":titleId, "title":title, "preWatch": watchedNumbers}})
+function addPreEpi (titleId, title, episode){
+    chrome.storage.local.get(titleId, (titleIdObjs) => {
+        Object.entries(titleIdObjs).forEach((titleIdObj) => {
+            if (titleIdObj[1]) {
+                const watchedEpi = titleIdObj[1].preWatch
+                if (!watchedEpi.includes(episode)) {
+                    watchedEpi.push(episode)
+                    chrome.storage.local.set({[titleId]:{"id":titleId, "title":title, "preWatch": watchedEpi}})
                     // chrome.storage.local.get(titleId, (test) => {
                     //     console.log("preWatch 확인용",test)
                     // })
                 }
             }
             else {
-                chrome.storage.local.set({[titleId]:{"id":titleId, "title":title, "preWatch":[no]}})
+                chrome.storage.local.set({[titleId]:{"id":titleId, "title":title, "preWatch":[episode]}})
             }
         })
 
@@ -108,13 +97,11 @@ function isWatched(id) {
         console.log(contentLsit, target)
 
         Object.entries(target).forEach((keys) => {
-            // console.log(keys[1].preWatch)
             const compare1 = keys[1].preWatch
             contentLsit.forEach((temp) => {
                 const aaa = temp.querySelector("td a")
                 const tempUrl = new URL(aaa.href)
                 const num = tempUrl.searchParams.get('no')
-                // console.log(tempUrl, num)
                 if (compare1.includes(num)) {
                     temp.style.background="#F1EFDC"
                 }
